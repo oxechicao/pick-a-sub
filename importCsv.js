2781948ab5b08bfa2dc6e0eb4872fb31f6dc4315
Chicão Thiago <fthiagogv@gmail.com>
2021-04-27 20:31:36 -0300

    :bug: corrigindo bug da imagem

--- importCsv.js
+++ importCsv.js
@@ -15,14 +15,17 @@ document.getElementById('subs-csv')
       const data = dataSplit.reduce((acc, curr, idx) => {
         if (idx === 0 || idx >= (dataSplit.length - 2)) return acc;
         if (idx === 1) acc = [];
-        if (!acc.find(i => i[0] && i[0] === curr[0])) acc.push([curr[0], curr[1]]);
+        if (!acc.find(i => i[0] && i[0] === curr[0])) acc.push({ name: curr[0], date: curr[1] });
         return acc;
       }, []);
 
 
       const jsonData = JSON.stringify(data);
-
       localStorage.setItem('cht-subs-csv', jsonData);
+
+      const jsonSort = data.map((item, index) => index);
+      localStorage.setItem('cht-subs-sort', JSON.stringify(jsonSort));
+
       renderSubs(data);
     };
 
